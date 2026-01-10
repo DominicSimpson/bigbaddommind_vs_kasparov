f0738d201f9ac62d4d1eb0b86c0ce3e91aaaf635
db_simpson@yahoo.co.uk <db_simpson@yahoo.co.uk>
2026-01-10 21:49:24 +0000

    Completed piece movement logic generators

--- src/board/ChessBoard.ts
+++ src/board/ChessBoard.ts
@@ -20,6 +20,7 @@ export class ChessBoard {
         this.squares[rank][file].piece = piece; // Places piece on square
     }
 
+    // -------------------------------------------------------------------
 
     // Code below sets up all 32 pieces on board in initial position
     /**
@@ -77,10 +78,10 @@ export class ChessBoard {
 
         switch (piece.type) {
             case "pawn":    return this.pawnMoves(fromRank, fromFile, piece);
-            case "rook":    return [];
+            case "rook":    return this.rookMoves(fromRank, fromFile, piece);
             case "knight":  return this.knightMoves(fromRank, fromFile, piece);
-            case "bishop":  return [];
-            case "queen":   return [];
+            case "bishop":  return this.bishopMoves(fromRank, fromFile, piece);
+            case "queen":   return this.queenMoves(fromRank, fromFile, piece);
             case "king":    return this.kingMoves(fromRank, fromFile, piece);
         }
     }
@@ -124,6 +125,54 @@ export class ChessBoard {
             }
         }
 
+    //------------------------------------------------------------------------------------------------
+
+    // //Helper function for ray-based sliding logic:
+    // pieces that move by repeated steps in a straight line until something stops them
+    // (either another piece or the end of the board), with each direction one ray (like a laser beam!)
+    // //Pieces:
+    // Rook → straight lines
+    // Bishop → diagonals
+    // Queen → straight lines + diagonals
+
+    // dr = delta rank - change in rank (first number)
+    // df = delta file - change in file (second number)
+    private rayMoves(r: Rank, f: File, piece: Piece, directions: Array<[number, number]>): Move[] {
+        // r: the piece's current rank (0–7)
+        // f: the piece's current file (0–7)
+        const moves: Move[] = []; // creates empty array to store moves pieces will make
+
+        for (const[dr, df] of directions) { // Loops over each direction the piece can move in
+            // directions is an array of (dr, df) direction offsets
+            // Initializes the first square one step away from the starting square in the current direction:
+            let rr = r + dr; // rr = the "ray cursor" - will keep moving outward until stopped
+            let ff = f + df; // ff = ditto
+
+            while(this.inBounds(rr, ff)) { // loop checks if piece can continue moving outward
+                const tr = rr as Rank;
+                const tf = ff as File;
+
+                const target = this.getSquare(tr, tf).piece; // checks what's on current square
+
+                if (target === null) { // confirms that square is empty, so piece can move here
+                    moves.push({ fromRank: r, fromFile: f, toRank: tr, toFile: tf });
+                } else {
+                    if (target.colour !== piece.colour) { // confirms square is occupied by enemy
+                        moves.push({ fromRank: r, fromFile: f, toRank: tr, toFile: tf, capture: true });
+                    }
+                    break; // ray stops either because square is friendly piece, or capture of enemy piece ends movement
+                }
+
+                // continue incrementally moving the ray one more square in same direction:
+                rr += dr;
+                ff += df;
+            }
+        }
+
+        return moves;
+    }
+
+    // --------------------------------------------------------------------- 
 
     // The following logic applies to all the following piece generators:
     // // Rank:
@@ -133,14 +182,17 @@ export class ChessBoard {
         // moving right - positive
         // moving left - negative
 
-    // r + 1: one rank up
-    // r - 1: one rank down
-    // f + 1: one file right
-    // f - 1: one file left
+    // r + 1: one rank up (north)
+    // r - 1: one rank down (south)
+    // f + 1: one file right (east)
+    // f - 1: one file left (west)
 
     // dr = delta rank - change in rank (first number)
     // df = delta file - change in file (second number)
 
+    // ----------------------------------------------------------------------
+
+    // pawn movement
 
     private pawnMoves(r: Rank, f: File, piece: Piece): Move[] {
         // r: the pawns' current rank (0–7)
@@ -151,7 +203,7 @@ export class ChessBoard {
         // otherwise pawn is black and therefore moves "negative" (down)
         // (obviously assuming white pieces start at bottom of board and black on top, as conventional)
         // ternary operator can be used because of type definition specifying only black or white:
-        const dir = piece.colour == "white" ? 1 : -1; // dir = direction
+        const dir = piece.colour === "white" ? 1 : -1; // dir = direction
 
         // starting position for both sets of pawns:
         const startRank = piece.colour === "white" ? 1 : 6;
@@ -167,11 +219,11 @@ export class ChessBoard {
 
         if (r === startRank && this.isEmpty(twoStepR, f)) { // checks if move is valid
             this.pushIfOk(moves, r, f, twoStepR, f, piece.colour);
+            }
         }
-    }
 
 
-        // accounts for pawn being able to capture opponent piece diagonally left or right
+        // accounts for pawn being able to capture opponent piece diagonally left or right up
         for (const df of [-1, +1]) {
             const capR = r + dir;
             const capF = f + df;
@@ -183,17 +235,47 @@ export class ChessBoard {
         
         return moves;
     }
+
+    // // Back row:
+
+    // rook movement
+
+    private rookMoves(r: Rank, f: File, piece: Piece): Move[] {
+        return this.rayMoves(r, f, piece, [
+            [+1,0], // up (north)
+            [-1,0], // down (south)
+            [0,+1], // right (east)
+            [0,-1]  // left (west)
+        ]);
+    }
+
+    // bishop movement
+
+    private bishopMoves(r: Rank, f: File, piece: Piece): Move[] {
+        return this.rayMoves(r, f, piece, [
+            [+1, +1], // diagonal north-east
+            [+1, -1], // diagonal north-west
+            [-1, +1], // diagonal south-east
+            [-1, -1]  // diagonal south-west
+        ])
+    }
     
+    // knight movement
+
     private knightMoves(r: Rank, f: File, piece: Piece): Move[] {
         // r: the knights' current rank (0–7)
         // f: the knights' current file (0–7)
         const moves: Move[] = []; // creates empty array to store moves knight will make
         const jumps: Array<[number, number]> = [
         // A knight always moves in a 2 + 1 pattern, with exactly 8 permutations of movement:
-            [-2, -1], [-2, +1], // two ranks down, one file left / two ranks down, one file right
-            [-1, -2], [-1, +2],  // one rank down, two files left / one rank down, two files right
-            [+1, -2], [+1, +2], // one rank up, two files left / one rank up, two files right  
-            [+2, -1], [+2, +1] // two ranks up, one file left / two ranks up, one file right
+            [-2, -1], // two ranks down, one file left
+            [-2, +1], // two ranks down, one file right
+            [-1, -2], // one rank down, two files left
+            [-1, +2], // one rank down, two files right
+            [+1, -2], // one rank up, two files left 
+            [+1, +2], // one rank up, two files right  
+            [+2, -1], // two ranks up, one file left
+            [+2, +1]  // two ranks up, one file right
         ];
 
         for (const [dr, df] of jumps) { // each pair of the above possible moves is:
@@ -204,21 +286,22 @@ export class ChessBoard {
         return moves;
     }
 
+    // king movement
     
     private kingMoves(r: Rank, f: File, piece: Piece): Move[] {
         // r: the kings' current rank (0–7)
         // f: the kings' current file (0–7)
         const moves: Move[] = []; // creates empty array to store moves king will make
         // // Movement permutations:
-        // ( 1,-1): up-left
-        // ( 1, 0): up
-        // ( 1, 1): up-right
-        // ( 0,-1): left
+        // ( 1,-1): north-west
+        // ( 1, 0): north
+        // ( 1, 1): north-east
+        // ( 0,-1): west
         // ( 0, 0): no move
-        // ( 0, 1): right
-        // ( 1,-1): down-left
-        // ( 1, 0): down
-        // ( 1, 1): down-right
+        // ( 0, 1): east
+        // (-1,-1): south-west
+        // (-1, 0): south
+        // (-1, 1): south-east
         
         // Starts an outer loop over delta rank (dr), i.e. vertical movement:
         for (let dr = -1; dr <= 1; dr++) { // -1: one rank down; +1: one rank up
@@ -231,6 +314,25 @@ export class ChessBoard {
         }
         return moves;
     }
+
+    // queen movement
+
+    private queenMoves(r: Rank, f: File, piece: Piece): Move[] {
+        return this.rayMoves(r, f, piece, [
+            [+1,  0], // north
+            [-1,  0], // south
+            [ 0, +1], // east
+            [ 0, -1], // west
+            [+1, +1], // diagonal north-east
+            [+1, -1], // diagonal north-west
+            [-1, +1], // diagonal south-east
+            [-1, -1]  // diagonal south-west
+        ]);
+    }
+
+
+    //---------------------------------------------------------------------------------
+
 
     public movePiece(move: Move): void {
         const fromSquare = this.getSquare(move.fromRank, move.fromFile);
