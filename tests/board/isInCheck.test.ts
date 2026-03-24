289dcb4fdfdc8b91b356f24d4a43865241c4af6f
db_simpson@yahoo.co.uk <db_simpson@yahoo.co.uk>
2026-03-24 19:22:39 +0000

    In check testing provisionally done, but will refactor code

--- tests/board/isInCheck.test.ts
+++ tests/board/isInCheck.test.ts
@@ -77,17 +77,38 @@ describe('vertical check detection', () => {
 
 describe('horizontal check detection', () => {
 
-    it('detects that a white queen on b8 checks a black king on d8', () => {
+    it('detects that white queen on a8 does not check black king on d8 if there is a black bishop on c8', () => {
         const board = new ChessBoard();
-        board.loadFEN('Q3k3/8/8/8/8/8/8/3K4 b - - 0 1');
+        board.loadFEN('Q1bk4/8/8/8/8/8/8/3K4 b - - 0 1');
+        expect(board.isInCheck('black')).toBe(false);
+    });
+    it('detects that a white queen on a8 checks a black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('Q2k4/8/8/8/8/8/8/3K4 b - - 0 1');
         expect(board.isInCheck('black')).toBe(true);
     });
+    it('detects that white queen on a7 does not check black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/Q7/8/8/8/8/8/3K4 b - - 0 1');
+        expect(board.isInCheck('black')).toBe(false);
+    });
 
+
+    it('detects that black queen on a1 does not check white king on d1 if there is white bishop on c1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/8/8/8/8/8/q1bK4 w - - 0 1');
+        expect(board.isInCheck('white')).toBe(false);
+    });
     it('detects that a black queen on a1 checks a white king on d1', () => {
         const board = new ChessBoard();
         board.loadFEN('3k4/8/8/8/8/8/8/q3K3 w - - 0 1');
         expect(board.isInCheck('white')).toBe(true);
     })
+     it('detects that black queen on a2 does not check black king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/8/8/8/8/q7/3K4 w - - 0 1');
+        expect(board.isInCheck('black')).toBe(false);
+    });
 
 });
 
@@ -98,11 +119,77 @@ describe('knight check detection', () => {
         board.loadFEN('3k4/8/4N3/8/8/8/8/3K4 b - - 0 1');    
         expect(board.isInCheck('black')).toBe(true);
     });
+    it('detects that white knight on d6 does not check black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/3N4/8/8/8/8/3K4 b - - 0 1');    
+        expect(board.isInCheck('black')).toBe(false);
+    });
 
-     it('detects that a black knight on c3 checks a white king on d1', () => {
+
+    it('detects that a black knight on c3 checks a white king on d1', () => {
         const board = new ChessBoard();
         board.loadFEN('3k4/8/8/8/8/2n5/8/3K4 w - - 0 1');    
         expect(board.isInCheck('white')).toBe(true);
+    });
+    it('detects that black knight on d3 does not check white king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/8/8/8/3n4/8/3K4 w - - 0 1');    
+        expect(board.isInCheck('white')).toBe(false);
+    });
+
+});
+
+describe('pawn check detection', () => {
+
+    it('detects that a white pawn on e7 checks a black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/4P3/8/8/8/8/8/3K4 b - - 0 1');    
+        expect(board.isInCheck('black')).toBe(true);
+    });
+      it('detects that white pawn on d7 does not check black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/3P4/8/8/8/8/8/3K4 b - - 0 1');    
+        expect(board.isInCheck('black')).toBe(false);
+    });
+
+
+    it('detects that a black pawn on c2 checks a white king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/8/8/8/8/2p5/3K4 w - - 0 1');    
+        expect(board.isInCheck('white')).toBe(true);
+    });
+    it('detects that a black pawn on d2 does not check white king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/8/8/8/8/3p4/3K4 w - - 0 1');    
+        expect(board.isInCheck('white')).toBe(false);
+
+    });
+
+});
+
+describe('king check detection', () => {
+
+    it('detects that a white king on c7 checks a black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/2K5/8/8/8/8/8/8 b - - 0 1');    
+        expect(board.isInCheck('black')).toBe(true);
+    });
+    it('detects that white king on d6 does not check black king on d8', () => {
+        const board = new ChessBoard();
+        board.loadFEN('3k4/8/3K4/8/8/8/8/8 b - - 0 1');    
+        expect(board.isInCheck('black')).toBe(false);
+    });
+
+
+    it('detects that a black king on e2 checks a white king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('8/8/8/8/8/8/4k3/3K4 w - - 0 1');    
+        expect(board.isInCheck('white')).toBe(true);
+    });
+    it('detects that black king on d3 does not check white king on d1', () => {
+        const board = new ChessBoard();
+        board.loadFEN('8/8/8/8/8/3k4/8/3K4 w - - 0 1');    
+        expect(board.isInCheck('white')).toBe(false);
     });
 
 });
